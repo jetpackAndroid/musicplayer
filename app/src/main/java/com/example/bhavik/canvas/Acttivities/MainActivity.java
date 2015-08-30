a0fc5fc4d2fa6c28514a5adb9ef12e8a5a44f9fb
Bhavik Javia <javiabhavik1@gmail.com>
2015-08-30 22:17:30 +0530

    Fragments Updated

--- app/src/main/java/com/example/bhavik/canvas/Acttivities/MainActivity.java
+++ app/src/main/java/com/example/bhavik/canvas/Acttivities/MainActivity.java
@@ -28,9 +28,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-public class MainActivity extends ActionBarActivity implements SongAdapter.customButtonListener, MediaController.MediaPlayerControl{
+public class MainActivity extends ActionBarActivity implements MusicFragment.passSongList{
 
-    MusicService musicService;
+    public static MusicService musicService;
     Intent playIntent;
     boolean musicBound = false;
     private ArrayList<Songs> songList;
@@ -43,87 +43,19 @@ public class MainActivity extends ActionBarActivity implements SongAdapter.custo
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        songsView = (ListView) findViewById(R.id.song_list);
-        songList = new ArrayList<Songs>();
-        getSongList();
-        Collections.sort(songList, new Comparator<Songs>() {
-            @Override
-            public int compare(Songs songs, Songs t1) {
-                return songs.getTitle().compareTo(t1.getTitle());
-            }
-        });
-        songAdapter = new SongAdapter(this, songList, MainActivity.this);
-        songsView.setAdapter(songAdapter);
+        setMainActivity(this);
         MusicFragment musicFragment = new MusicFragment();
         getFragmentManager().beginTransaction().replace(R.id.mainContainer, musicFragment,MusicFragment.TAG).commit();
-        setController();
-    }
-    private void setController(){
-        //set the controller up
-        controller = new MusicController(this);
-        controller.setPrevNextListeners(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                playNext();
-            }
 
-        }, new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                playPrev();
-            }
-        });
-        controller.setMediaPlayer(this);
-        controller.setAnchorView(findViewById(R.id.song_list));
-        controller.setEnabled(true);
-    }
-    //play next
-    private void playNext(){
-        musicService.playNext();
-        if(playbackPaused){
-            setController();
-            playbackPaused = false;
-        }
-        controller.show(0);
-    }
 
-    //play previous
-    private void playPrev(){
-        musicService.playPrev();
-        if(playbackPaused){
-            setController();
-            playbackPaused=false;
-        }
-        controller.show(0);
     }
-
-    public void getSongList(){
-//        We  can have _ID, ARTIST, TITLE, DISPLAY_NAME, DATA, ALBUM_ID, DURATION
-//        String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
-        ContentResolver contentResolver = getContentResolver();
-        Uri musicUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-        Cursor musicCursor = contentResolver.query(musicUri,null,null,null,null);
-        if(musicCursor != null && musicCursor.moveToFirst()){
-            int titleColumn = musicCursor.getColumnIndex(android.provider.MediaStore.Audio.Media.TITLE);
-            int idColumn = musicCursor.getColumnIndex(android.provider.MediaStore.Audio.Media._ID);
-            int artistColumn = musicCursor.getColumnIndex(android.provider.MediaStore.Audio.Media.ARTIST);
-            do{
-                long thisId = musicCursor.getLong(idColumn);
-                String thisTitle = musicCursor.getString(titleColumn);
-                String thisArtist = musicCursor.getString(artistColumn);
-                songList.add(new Songs(thisId, thisTitle, thisArtist));
-            }while(musicCursor.moveToNext());
-        }
-    }
-
     private ServiceConnection musicConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
             MusicService.MusicBinder binder = (MusicService.MusicBinder)iBinder;
             //get service
             musicService = binder.getService();
-            //pass list
-            musicService.setMusicList(songList);
+
             musicBound = true;
         }
         @Override
@@ -134,25 +66,7 @@ public class MainActivity extends ActionBarActivity implements SongAdapter.custo
     };
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        paused = true;
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        paused = false;
-    }
-
-    @Override
-    protected void onStop() {
-        controller.hide();
-        super.onStop();
-    }
-
-    @Override
-    protected void onStart() {
+    public void onStart() {
         super.onStart();
         if(playIntent==null){
             playIntent = new Intent(this, MusicService.class);
@@ -162,7 +76,7 @@ public class MainActivity extends ActionBarActivity implements SongAdapter.custo
     }
 
     @Override
-    protected void onDestroy() {
+    public void onDestroy() {
         stopService(playIntent);
         musicService = null;
         super.onDestroy();
@@ -199,83 +113,6 @@ public class MainActivity extends ActionBarActivity implements SongAdapter.custo
 
         return super.onOptionsItemSelected(item);
     }
-    @Override
-    public void onButtonClickListner(int position, String value) {
-//        Toast.makeText(MainActivity.this, "hi" + position, Toast.LENGTH_LONG).show();
-        musicService.setSong(position);
-        musicService.playSong();
-        if(playbackPaused){
-            setController();
-            playbackPaused=false;
-        }
-        controller.show(0);
-    }
-
-    @Override
-    public void start() {
-        musicService.go();
-    }
-
-    @Override
-    public void pause() {
-        playbackPaused=true;
-        musicService.pausePlayer();
-    }
-
-    @Override
-    public int getDuration() {
-        if(musicService != null && musicBound && musicService.isPng())
-            return musicService.getDur();
-        else
-            return 0;
-    }
-
-    @Override
-    public int getCurrentPosition() {
-        if(musicService != null && musicBound && musicService.isPng())
-            return musicService.getPosn();
-        else
-            return 0;
-    }
-
-    @Override
-    public void seekTo(int pos) {
-        musicService.seek(pos);
-    }
-
-    @Override
-    public boolean isPlaying() {
-        if(musicService != null && musicBound)
-            return musicService.isPng();
-        else
-            return false;
-
-    }
-
-    @Override
-    public int getBufferPercentage() {
-        return 0;
-    }
-
-    @Override
-    public boolean canPause() {
-        return true;
-    }
-
-    @Override
-    public boolean canSeekBackward() {
-        return true;
-    }
-
-    @Override
-    public boolean canSeekForward() {
-        return true;
-    }
-
-    @Override
-    public int getAudioSessionId() {
-        return 0;
-    }
 
     public static MainActivity getMainActivity() {
         return mainActivity;
@@ -285,5 +122,11 @@ public class MainActivity extends ActionBarActivity implements SongAdapter.custo
 
     public static void setMainActivity(MainActivity mainActivity) {
         MainActivity.mainActivity = mainActivity;
+    }
+
+    @Override
+    public void completeSongList(ArrayList<Songs> songList) {
+        //pass list
+        musicService.setMusicList(songList);
     }
 }
